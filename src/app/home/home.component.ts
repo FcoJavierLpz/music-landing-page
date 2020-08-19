c62f29b2e643d36aded85b6a58c1f8b96f29f076
Francisco J <flopez@linko.mx>
2020-08-18 21:46:39 -0500

    feat: adding background particles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as  particlesJS from 'particlesjs';
 
+declare var particlesJS: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +12,9 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    particlesJS.load('particles-js', 'assets/particles.json', function() {
+      console.log('callback - particles.js config loaded');
+    });
   }
 
 }
